200ff23fbc9ef464e7e5e980b5b9cb4df057a29f
John Scott <johnrscott0@gmail.com>
2024-01-21 12:24:15 +0000

    Added script to create clock IP

--- serial/vivado/run.tcl
+++ serial/vivado/run.tcl
@@ -1,3 +1,9 @@
 create_project -in_memory -part xc7a35ticsg324-1L
 
-read_ip clock_clk_wiz_0_0.xci
+read_verilog -sv [ glob ../src/*.sv ]
+read_verilog hello_world_wrapper.v
+read_xdc constraints.xdc
+read_ip ./.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci
+
+generate_target all [get_ips]
+
