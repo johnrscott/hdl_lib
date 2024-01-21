dd85be6f587d348dee54dbe75504b6592b7460ed
John Scott <johnrscott0@gmail.com>
2024-01-21 13:26:48 +0000

    Checked the script works from clean

--- serial/vivado/hello_world_wrapper.v
+++ serial/vivado/hello_world_wrapper.v
@@ -6,21 +6,21 @@ module hello_world_wrapper(
     wire clk_internal;
 
    // Signal names as documented in IP documentation for clocking wizard?
-   // clk_wiz_0 clk_wiz_0(
-   //    .clk_in1(clk),
-   //    .clk_out1(clk_internal)
-   // );
+   clk_wiz_0 clk_wiz_0(
+      .clk_in1(clk),
+      .clk_out1(clk_internal)
+   );
 
    // Copied from instantiation template
-   clk_wiz_0 instance_name (
-      // Clock out ports
-      .clk_out1(clk_internal),     // output clk_out1
-      // Status and control signals
-      .reset(reset), // input reset
-      .locked(locked),       // output locked
-      // Clock in ports
-      .clk_in1(clk)      // input clk_in1
-   );
+   // clk_wiz_0 instance_name (
+   //    // Clock out ports
+   //    .clk_out1(clk_internal),     // output clk_out1
+   //    // Status and control signals
+   //    .reset(reset), // input reset
+   //    .locked(locked),       // output locked
+   //    // Clock in ports
+   //    .clk_in1(clk)      // input clk_in1
+   // );
    
    
     assign tx_debug = tx;
--- serial/vivado/run.tcl
+++ serial/vivado/run.tcl
@@ -3,17 +3,16 @@ create_project -in_memory -part xc7a35ticsg324-1L
 read_verilog -sv [ glob ../src/*.sv ]
 read_verilog hello_world_wrapper.v
 read_xdc constraints.xdc
-read_ip ./.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci
 
 # Copied from Vivado
 # set_property board_part digilentinc.com:arty-a7-35:part0:1.1
 
-# Copied from Vivado (clock 100MHz without reset)
-# create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_wiz_0
-# set_property CONFIG.USE_RESET {false} [get_ips clk_wiz_0]
+# THIS: clock 100MHz without reset
+create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_wiz_0
+set_property CONFIG.USE_RESET {false} [get_ips clk_wiz_0]
 
-# Copied from Vivado (is this important?)
-generate_target {instantiation_template} [get_ips]
+# OR...
+read_ip ./.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci
 
 # Seems like this also generates instantiation template
 generate_target all [get_ips]
