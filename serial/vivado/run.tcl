ca77b65b5c6cb8275e00253a0a711cabb5d16c52
John Scott <johnrscott0@gmail.com>
2024-01-21 13:21:06 +0000

    Found the issue (missing synth_ip). Now RTL synthesizes

--- serial/vivado/run.tcl
+++ serial/vivado/run.tcl
@@ -5,7 +5,27 @@ read_verilog hello_world_wrapper.v
 read_xdc constraints.xdc
 read_ip ./.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci
 
+# Copied from Vivado
+# set_property board_part digilentinc.com:arty-a7-35:part0:1.1
+
+# Copied from Vivado (clock 100MHz without reset)
+# create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_wiz_0
+# set_property CONFIG.USE_RESET {false} [get_ips clk_wiz_0]
+
+# Copied from Vivado (is this important?)
+generate_target {instantiation_template} [get_ips]
+
+# Seems like this also generates instantiation template
 generate_target all [get_ips]
+
+# Will generate output products if needed. This is crucial --
+# If you skip this step, Vivado will give module-not-found errors
+# when attempting to synthesize the user RTL design and IP instantiation
+# templates are used in the RTL.
+synth_ip [get_ips]
+
+# Copied from Vivado (does not seem to fix issue)
+export_ip_user_files -of_objects [get_ips] -no_script -sync -force -quiet
 
 synth_design -top hello_world_wrapper
 
