66ffbe0c6c023eb60a44f5152739f5accaf03c26
John Scott <johnrscott0@gmail.com>
2024-01-26 22:40:42 +0000

    Got initial version of the debouncing working

--- debug_buttons/src/debug_buttons.sv
+++ debug_buttons/src/debug_buttons.sv
@@ -1,12 +1,74 @@
-module debug_buttons(
+module debug_buttons #(
+   // 50 ms debouncing (mainly for slide switches) assuming 100MHz clock
+   parameter DEBOUNCE_PERIOD = 5//_000_000
+)(
    input [3:0]	       buttons, switches,
    wishbone.controller wb
 );
 
+   // After one flip-flop, *_meta may be metastable,
+   // and after second flip-flop, *_sync should be
+   // synchronous with the wb.clk_i
+   logic [3:0] buttons_meta = 0, buttons_sync = 0;
+   logic [3:0] switches_meta = 0, switches_sync = 0;
+
+   // This design will debounce the whole 8-bit input
+   // simultaneously, by only storing changes to the
+   // debounced register when it has been stable for
+   // at least DEBOUNCE_PERIOD
+   logic [31:0]	debounce_counter = 0;
+   logic [7:0]	input_sync, input_tmp = 0, input_debounce = 0;;
+   assign input_sync = { switches_sync, buttons_sync };
    
+   always_ff @(posedge wb.clk_i) begin: synchronize
+      if (wb.rst_i) begin
+	 buttons_meta <= 0;
+	 buttons_sync <= 0;
+	 switches_meta <= 0;
+	 switches_sync <= 0;
+      end
+      else begin
+	 buttons_meta <= buttons;
+	 buttons_sync <= buttons_meta;
+	 switches_meta <= switches;
+	 switches_sync <= switches_meta;  
+      end
+   end
    
+   always_ff @(posedge wb.clk_i) begin: debounce
+      if (wb.rst_i) begin
+	 input_debounce <= 0;
+	 debounce_counter <= 0;
+      end
+      else if (debounce_counter == DEBOUNCE_PERIOD) begin
+	 debounce_counter <= 0;
+	 if (input_sync == input_tmp)
+	   input_debounce <= input_tmp;
+      end
+      else if (debounce_counter != 0)
+	debounce_counter <= debounce_counter + 1;
+      else if (input_sync != input_debounce) begin
+	 debounce_counter <= 1;
+	 input_tmp <= input_sync;
+      end
+   end
+
+
+`ifdef FORMAL
+
+   default clocking @(posedge wb.clk_i);
+   endclocking //
+
+   default disable iff (wb.rst_i);
+
+   sequence button_change_no_bounce;
+      $stable(switches) throughout ($stable(buttons)[*25] ##1 $changed(buttons) ##1 $stable(buttons)[*25]) 
+   endsequence
    
+   no_bounce_change: cover property (button_change_no_bounce |-> ##[0:55] $changed(input_debounce));
+
    
- 
+`endif
+   
 endmodule
   
--- debug_buttons/src/wishbone.sv
+++ debug_buttons/src/wishbone.sv
@@ -6,12 +6,12 @@ interface wishbone #(parameter DAT_WIDTH = 8)(input logic clk_i, rst_i);
    	 
    modport controller(
       output cyc_o, stb_o, we_o,
-      input  ack_i, err_i, rty_i, stall_i
+      input  clk_i, rst_i, ack_i, err_i, rty_i, stall_i
    );
 
    modport device(
       output .ack_o(ack_i), .err_o(err_i), .rty_o(rty_i), .stall_o(stall_i),
-      input .cyc_i(cyc_o), .stb_i(stb_o), .we_i(we_o)
+      input clk_i, rst_i, .cyc_i(cyc_o), .stb_i(stb_o), .we_i(we_o)
    );
    
 endinterface
