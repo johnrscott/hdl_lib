b1faaf118aa46ce3d3a9f32b77c7b381e80b2f21
John Scott <johnrscott0@gmail.com>
2024-01-26 20:19:15 +0000

    Started working on a new wishbone debugging module

---
+++ debug_buttons/src/debug_buttons.sv
@@ -0,0 +1,12 @@
+module debug_buttons(
+   input [3:0]	       buttons, switches,
+   wishbone.controller wb
+);
+
+   
+   
+   
+   
+ 
+endmodule
+  
---
+++ debug_buttons/src/wishbone.sv
@@ -0,0 +1,17 @@
+interface wishbone #(parameter DAT_WIDTH = 8)(input logic clk_i, rst_i);
+   
+   // From perspective of controller
+   logic cyc_o, stb_o, we_o, ack_i, err_i, rty_i, stall_i;
+   logic [DAT_WIDTH-1:0] dat_o;
+   	 
+   modport controller(
+      output cyc_o, stb_o, we_o,
+      input  ack_i, err_i, rty_i, stall_i
+   );
+
+   modport device(
+      output .ack_o(ack_i), .err_o(err_i), .rty_o(rty_i), .stall_o(stall_i),
+      input .cyc_i(cyc_o), .stb_i(stb_o), .we_i(we_o)
+   );
+   
+endinterface
