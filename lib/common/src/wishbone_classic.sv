25006d8bb5c95948003a68fd619d14e8ed1627e8
John Scott <johnrscott0@gmail.com>
2024-02-03 18:29:17 +0000

    Added wishbone classic interface and controller module

---
+++ lib/common/src/wishbone_classic.sv
@@ -0,0 +1,28 @@
+interface wishbone_classic #(
+   parameter DAT_WIDTH = 8
+)(
+   input logic clk_i, rst_i
+);
+
+   // From perspective of controller
+   logic cyc_o, stb_o, we_o, ack_i, err_i, rty_i;
+   logic [DAT_WIDTH-1:0] dat_o;
+
+   initial begin
+      cyc_o = 0;
+      stb_o = 0;
+      we_o = 0;
+      dat_o = 0;
+   end
+   
+   modport controller(
+      output cyc_o, stb_o, we_o, dat_o,
+      input  clk_i, rst_i, ack_i, err_i, rty_i
+   );
+
+   modport device(
+      output .ack_o(ack_i), .err_o(err_i), .rty_o(rty_i),
+      input clk_i, rst_i, .cyc_i(cyc_o), .stb_i(stb_o), .we_i(we_o), .dat_i(dat_o)
+   );
+      
+endinterface
---
+++ lib/common/src/wishbone_ctrl_classic.sv
@@ -0,0 +1,50 @@
+/// Simple Wishbone Classic Read/Write Cycle Controller
+///
+/// This module implements the Wishbone B4 specification
+/// classic single read/write cycle.
+///
+/// A write transaction is prepared by setting write_data,
+/// setting write_en, and asserting start. The cyc signal
+/// is set while the cycle is underway, and ack is set to
+/// indicate cycle completion.
+///
+// A read transaction is prepared by clearing write_en, and
+/// asserting start. The cyc signal is set while the transaction
+/// is underway, and ack is set to indicate that read_data
+/// contains valid data.
+///
+/// Multiple transactions can be performed back to back by
+/// keeping start asserted. In this case, data for the next
+/// transaction should be maintained until ack is asserted,
+/// at which point new write_data/write_en can be loaded (on
+/// the rising ack edge).
+///  
+module wishbone_ctrl_classic #(
+   parameter DAT_WIDTH = 8
+)(
+   output [DAT_WIDTH-1:0]      read_data,
+   output		       cyc, ack,
+   input [DAT_WIDTH-1:0]       write_data,
+   input		       write_en,
+   input		       start,
+   wishbone_classic.controller wb
+);
+
+   assign cyc = wb.cyc_o;
+   assign ack = wb.ack_i;
+   assign read_data = wb.dat_i;
+   
+   assign wb.cyc_o = sb.stb_o;
+   assign wb.we_o = write_en && wb.cyc_o;
+   assign wb.dat_o = write_data;
+   
+   always_ff @(posedge wb.clk_i) begin: start_transaction
+      if (wb.rst_i)
+	wb.stb_o <= 0;
+      else if (start)
+	wb.stb_o <= 1;
+      else if (wb.ack_i)
+	wb.stb_o <= 0;
+   end
+
+endmodule
