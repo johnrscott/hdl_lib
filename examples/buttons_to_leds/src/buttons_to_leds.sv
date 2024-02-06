31ab5027f6ae543d15d3ebba0ed2dec592a4517d
John Scott <johnrscott0@gmail.com>
2024-02-06 20:08:02 +0000

    Confirmed buttons_to_leds formal verification still passes
    Also added a define to restrict debounce period to 5 for formal verification

--- examples/buttons_to_leds/src/buttons_to_leds.sv
+++ examples/buttons_to_leds/src/buttons_to_leds.sv
@@ -1,5 +1,9 @@
 import types::rgb_led_t;
 
+`ifndef DEBOUNCE_PERIOD
+ `define DEBOUNCE_PERIOD 5_000_000
+`endif
+
 module buttons_to_leds(
    input	      clk_i, rst_i,
    input [3:0]	      buttons, switches,
@@ -7,10 +11,8 @@ module buttons_to_leds(
    output	      rgb_led_t [3:0] rgb_leds
 );
 
-   localparam DEBOUNCE_PERIOD = 5_000_000;
-   
    wishbone_classic wb(.clk_i, .rst_i);
-   buttons #(.DEBOUNCE_PERIOD(DEBOUNCE_PERIOD)) btns(.buttons, .switches, .wb);
+   buttons #(.DEBOUNCE_PERIOD(`DEBOUNCE_PERIOD)) btns(.buttons, .switches, .wb);
    leds leds(.green_leds, .rgb_leds, .wb);
    
 endmodule
